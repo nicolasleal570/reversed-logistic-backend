fce319b1a9f11bcef259618a399a21cf63cd2789
Nicolás Leal <lealnicolas96@gmail.com>
2022-06-04 16:04:24 -0400

    Fix: sequelize models config

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -3,4 +3,8 @@ const { DB_URI, config } = require('../config/environment');
 const { setupModels } = require('./models');
 const databaseConfig = require('./databases');
 
-export const sequelize = new Sequelize(DB_URI, databaseConfig[config.env]);
+const sequelize = new Sequelize(DB_URI, databaseConfig[config.env]);
+
+setupModels(sequelize);
+
+module.exports = { sequelize };
