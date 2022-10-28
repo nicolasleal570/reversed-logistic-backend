4db19da3c8eff0d5ac09f538d6a85d5633d31dd8
Nicolás Leal <lealnicolas96@gmail.com>
2022-10-28 12:05:09 -0400

    Fix truck service

--- src/services/trucks.service.js
+++ src/services/trucks.service.js
@@ -37,22 +37,20 @@ class TrucksService {
   async update(id, changes) {
     const truck = await this.findOne(id);
 
-    let res;
     if ('userId' in changes) {
       await this.userService.findOne(changes.userId);
-      res = await truck.update(changes);
-    } else {
-      res = await truck.update(changes);
     }
 
-    return res;
+    await truck.update(changes);
+
+    return this.findOne(id);
   }
 
   async delete(id) {
     const truck = await this.findOne(id);
     await truck.destroy();
 
-    return truck;
+    return this.findOne(id);
   }
 }
 
